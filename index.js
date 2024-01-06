a2ab501bbbe1c4f3da17c46d4120f668f8d6e11d
guptaPrateek10 <guptaPrateek10@gmail.com>
2024-01-06 14:20:41 +0530

    server js created, app logic is now seperated

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const dbConnect = require('./config/dbConfig');
-const authRoutes = require('./src/routes/auth');
-const userRoutes = require('./src/routes/user');
-const productRoute = require('./src/routes/product');
-
+const dbConnect = require("./config/dbConfig");
+const authRoutes = require("./src/routes/auth");
+const userRoutes = require("./src/routes/user");
+const productRoute = require("./src/routes/product");
 
 //connecting to Mongo db
 dbConnect();
 
-
 app.use(express.json());
-const PORT = process.env.PORT || 8000;
 
 //Define authentication routes
-app.use('/auth',authRoutes);
+app.use("/auth", authRoutes);
 
 //Define User Routes
-app.use('/user',userRoutes);
+app.use("/user", userRoutes);
 
 //Define product Roures
-app.use('/product',productRoute)
+app.use("/product", productRoute);
 
-app.listen(PORT,()=>{
-    console.log(`server is running on ${PORT}` )
-})
+module.exports = app;
